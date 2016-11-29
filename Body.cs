f1eda6538d429415e8e833d0b0d65fa7776f93e1
NightXsenator <lapinkirill@msn.com>
2016-11-29 15:06:51 +0300

    Add files via upload

---
+++ Body.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using System.Collections;
+
+namespace TestTaskAtrinity
+{
+    class Person : IComparable<Person>
+    {
+        string surname;
+        int heightcm;
+        DateTime birthdate;
+
+        public string Surname
+        {
+            get
+            {
+                return surname;
+            }
+        }
+        public int Heightcm
+        {
+            get
+            {
+                return heightcm;
+            }
+        }
+        public DateTime Birthdate
+        {
+            get
+            {
+                return birthdate;
+            }
+        }
+
+        public Person(string surname)
+        {
+            this.surname = surname;
+        }
+        public Person(string surname, int heightcm, int year, int month, int day) : this(surname)
+        {
+            this.heightcm = heightcm;
+            birthdate = new DateTime(year, month, day);
+        }
+        public override string ToString()
+        {
+            return "Surname: " + surname + ", height, in cm: " + heightcm + " birthdate: " + birthdate.ToLongDateString();
+        }
+        public int CompareTo(Person other)
+        {
+            return this.surname.CompareTo(other.surname);
+        }
+    }
+
+    class Node<t> : IComparable<Node<t>> // реализация узла двунаправленного связного списка
+ where t : class, IComparable<t>                 // из-за метода Remove пришлось сделать его публичным.
+    {
+        public t item;
+        public Node<t> next;
+        public Node<t> prev;
+
+        public Node(t init) { item = init; }
+        public int CompareTo(Node<t> other)
+        {
+            return (this.item.CompareTo(other.item));
+        }
+        public static Node<t> operator ++(Node<t> x) { x = x.next; return x; } // инкремент = следующий узел в списке.
+        public static Node<t> operator --(Node<t> x) { x = x.prev; return x; }
+    }
+
+    class NodeList<p> : IEnumerable<Node<p>>
+        where p : class, IComparable<p>
+    {
+        private Node<p> first;
+        public bool reverseiterator = false; // влияет на направление итерации по списку
+
+        private Node<p> Last
+        {
+            get
+            {
+                if (first == null) return null;
+                else
+                {
+                    Node<p> current = first;
+                    while (current.next != null) current++;
+                    return current;
+                }
+            }
+        } // свойство - последний элемент
+
+        private Node<p> nodeSeek(int ind)
+        {
+            Node<p> current = first;
+            for (int i = 0; i < ind; i++)
+            {
+                if (current.next == null || current == null) return null;
+                current++;
+            }
+            return current;
+        }
+        private void swapWithPrev(Node<p> y)
+        {
+            if (y == null || y.prev == null) return;
+            Node<p> x = y.prev;
+            /* X - это предшествующий Y элемент.
+             Следующие два условных оператора определяют преобразования связей элементов, соседствующих с парой XY.
+             */
+            if (x != first) { x.prev.next = y; y.prev = x.prev; }
+            else { first = y; y.prev = null; }
+
+            if (y.next != null) { y.next.prev = x; x.next = y.next; }
+            else x.next = null;
+
+            y.next = x;
+            x.prev = y;
+        }
+        public p seek(int ind)
+        {
+            if (nodeSeek(ind) == null) return default(p);
+            else return nodeSeek(ind).item;
+        }
+        public void addValue(p input)  // добавление элемента в конец списка
+        {
+            Node<p> entry = new Node<p>(input);
+            if (first == null) first = entry;
+            else
+            {
+                entry.prev = Last;
+                Last.next = entry;
+            }
+        }
+        public void addValue(p input, int ind) // добавление элемента с индексом ind со сдвигом остатка списка вправо
+        {
+            Node<p> entry = new Node<p>(input);
+            if (first == null) first = entry; // в случае пустого списка
+            else if (ind == 0) // вставка в начало списка
+            {
+                first.prev = entry;
+                entry.next = first;
+                first = entry;
+            }
+            else if (nodeSeek(ind) == null) // если замещаемого эл-та с указанным индексом не существует, 
+            {                               // то элемент добавляется в конец.
+                entry.prev = Last;
+                Last.next = entry;
+            }
+            else    // самый сложный случай: добавляемый элемент - не концевой
+            {
+                Node<p> current = nodeSeek(ind);
+                entry.prev = current.prev;
+                entry.prev.next = entry;
+                current.prev = entry;
+                entry.next = current;
+            }
+        }
+        public void remove(Node<p> current)
+        {
+            if (current == null) return; // элемент с несуществующим индексом => ф-ция ничего не делает
+            if (current == first) // удаляется элемент из начала списка (2 варианта: единственный и неединственный элемент)
+            {
+                if (first.next == null) { first = null; return; }
+                else
+                {
+                    first++;
+                    first.prev = null;
+                    return;
+                }
+            }
+            if (current.next == null) { current.prev.next = null; return; } // удаляется элемент из конца списка
+            else                                            // удаляется элемент, который не в начале и не в конце списка
+            {
+                current.prev.next = current.next;
+                current.next.prev = current.prev;
+            }
+        }
+        public void removeAt(int f)            // удаление элемента с индексом f
+        {
+            Node<p> current = nodeSeek(f);
+            remove(current);
+        }
+        public void removeAll()
+        {
+            first = null;
+        }
+        public IEnumerator<Node<p>> GetEnumerator()
+        {
+            Node<p> current;
+            if (reverseiterator == false)
+            {
+                current = first;
+                while (current != null) { yield return current; current = current.next; }
+            }
+            else
+            {
+                current = Last;
+                while (current != null) { yield return current; current = current.prev; }
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+        public void sort() // сортировка вставками
+                           /*
+                           Во втором цикле присваивание j=j означает, что переход к предыдущему элементу в порядке списка
+                           осуществляется процедурой swapWithPrev, которая меняет местами два узла, но не изменяет подаваемой
+                           на вход ссылки. Индекс элемента с адресом j становится меньше на единицу. 
+                           Похожая причина и у присваивания i=i.next в теле цикла. 
+                           */
+        {
+            for (Node<p> i = first.next; i != null; i++)
+                for (Node<p> j = i; j.prev != null && j.prev.CompareTo(j) > 0; j = j)
+                {
+                    swapWithPrev(j);
+                    if (i == j) i++;
+                }
+        }
+
+    }
+}
---
+++ Head.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+namespace TestTaskAtrinity
+{
+    class Head
+    {
+        public static void Main(string[] args)
+        {
+            FileStream S = new FileStream("Menu.txt", FileMode.Open); // текст меню из-за его длины я решил поместить в файл.
+            StreamReader s = new StreamReader(S);
+            Menu menu = new Menu(new NodeList<Person>(), s.ReadToEnd());
+            menu.viewMenu();
+            s.Close();
+        }        
+    }
+
+    class Menu
+    {
+        public Menu(NodeList<Person> L, string text)
+        {
+            this.text = text;
+            this.L = L;
+        }
+        string text;
+        NodeList<Person> L;
+
+        public void viewMenu()
+        {
+            Console.WriteLine(text);
+            retry: string x = Console.ReadLine();
+            switch (x)
+            {
+                case ("1"): viewPeople(false); break;
+                case ("2"): viewPeople(true); break;
+                case ("3"): addPerson(); break;
+                case ("4"): deletePerson(); break;
+                case ("5"): deletePeopleBySurname(); break;
+                case ("6"): sortList(); break;
+                case ("7"): saveToTxt(); break;
+                case ("8"): loadFromTxt(); break;
+                case ("9"): saveToXml(); break;
+                case ("0"): loadFromXml(); break;
+                case ("*"): return;
+                default: Console.WriteLine("Wrong entry. Please, try again."); goto retry;
+            }
+            Console.WriteLine("Done! Press any key...");
+            Console.ReadKey();
+            viewMenu();
+        }
+
+        private void viewPeople(bool reverse)
+        {
+            L.reverseiterator = reverse;
+            foreach (Node<Person> x in L) Console.WriteLine(x.item);
+        }
+        private void addPerson()
+        {
+            try
+            {
+                Console.WriteLine("Enter newcomer's future index number in the list (must be above zero):");
+                int i = Convert.ToInt32(Console.ReadLine());
+                if (i < 0) throw new InvalidDataException();
+                Console.WriteLine("Enter newcomer's surname:");
+                string surname = Console.ReadLine();
+                Console.WriteLine("Enter his height in cm, integer(!):");
+                int height = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter his year of birth:");
+                int year = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the number of month he was born in:");
+                int month = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Now, the day of birth:");
+                int day = Convert.ToInt32(Console.ReadLine());
+                Person newbie = new Person(surname, height, year, month, day);
+                L.addValue(newbie);
+            }
+            catch
+            {
+                Console.WriteLine("Sorry, invalid data. Returning to the main menu...\n");
+                viewMenu();
+            }
+        }
+        private void deletePerson()
+        {
+            Console.WriteLine("Enter index of an element to be deleted (must be above zero):");
+            int i = Convert.ToInt32(Console.ReadLine());
+            if (i < 0) throw new InvalidDataException();
+            L.removeAt(i);
+        }
+        private void deletePeopleBySurname()
+        {
+            Console.WriteLine("People with which surname should be removed from the list?");
+            string s = Console.ReadLine();
+            foreach (Node<Person> p in L) { if (p.item.Surname == s) L.remove(p); }
+        }
+        private void sortList()
+        {
+            L.sort();
+        }
+        private void saveToXml()
+        {
+            XmlDocument document = new XmlDocument();
+            document.AppendChild(document.CreateElement("people"));
+            XmlElement root = document.DocumentElement;
+            XmlElement person = document.CreateElement("person");
+            XmlElement surname = document.CreateElement("surname");
+            XmlElement height = document.CreateElement("height");
+            XmlElement birthdate = document.CreateElement("birthdate");
+            birthdate.AppendChild(document.CreateElement("year"));
+            birthdate.AppendChild(document.CreateElement("month"));
+            birthdate.AppendChild(document.CreateElement("day"));
+            person.AppendChild(surname); person.AppendChild(height); person.AppendChild(birthdate);
+            L.reverseiterator = false;
+            foreach (Node<Person> p in L)
+            {
+                XmlElement x = (XmlElement)person.CloneNode(true);
+                x.SelectSingleNode("//surname").InnerText = p.item.Surname;
+                x.SelectSingleNode("//height").InnerText = p.item.Heightcm.ToString();
+                x.SelectSingleNode("//year").InnerText = p.item.Birthdate.Year.ToString();
+                x.SelectSingleNode("//month").InnerText = p.item.Birthdate.Month.ToString();
+                x.SelectSingleNode("//day").InnerText = p.item.Birthdate.Day.ToString();
+                root.AppendChild(x);
+            }
+            document.Save("List.xml");
+        }
+        private void saveToTxt()
+        {
+            FileStream F = new FileStream("List.txt", FileMode.Create);
+            StreamWriter stream = new StreamWriter(F);
+            foreach (Node<Person> node in L)
+            {
+                stream.WriteLine(node.item.Surname + ',' + node.item.Heightcm + ',' + node.item.Birthdate.Year + ',' +
+                    node.item.Birthdate.Month + ',' + node.item.Birthdate.Day);
+            }
+            stream.Close(); F.Close();
+        }
+        private void loadFromTxt()
+        {
+            try
+            {
+                L.removeAll();
+                FileStream F = new FileStream("List.txt", FileMode.OpenOrCreate);
+                StreamReader stream = new StreamReader(F);
+                string line = stream.ReadLine();
+                string[] a;
+                while (line != null)
+                {
+                    a = line.Split(',');
+                    L.addValue(new Person(a[0], Convert.ToInt32(a[1]),
+                        Convert.ToInt32(a[2]), Convert.ToInt32(a[3]), Convert.ToInt32(a[4])));
+                    line = stream.ReadLine();
+                }
+                stream.Close(); F.Close();
+            }
+            catch
+            {
+                Console.WriteLine("Invalid or nonexisting file. Returning to the main menu...\n");
+                viewMenu();
+            }
+        }
+        private void loadFromXml()
+        {
+            try
+            {
+                XmlDocument document = new XmlDocument();
+                document.Load("List.xml");
+                L.removeAll();
+                XmlNode root = document.DocumentElement;
+                XmlNodeList people = root.SelectNodes("*");
+                foreach (XmlElement person in people)
+                {
+                    string surname = person.SelectSingleNode("//surname").InnerText;
+                    int height = Convert.ToInt32(person.SelectSingleNode("//height").InnerText);
+                    int year = Convert.ToInt32(person.SelectSingleNode("//year").InnerText);
+                    int month = Convert.ToInt32(person.SelectSingleNode("//month").InnerText);
+                    int day = Convert.ToInt32(person.SelectSingleNode("//day").InnerText);
+                    L.addValue(new Person(surname, height, year, month, day));
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Invalid or nonexisting file. Returning to the main menu...\n");
+                viewMenu();
+            }
+
+        }
+    }
+}
